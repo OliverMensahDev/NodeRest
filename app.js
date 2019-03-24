cbc47fb7d21fa8e37d9ca742a96359277632acbb
Oliver Mensah <olivermensah96@gmail.com>
2019-03-24 12:12:16 +0000

    Working with Hypermedia As The Engine Of Application State for REST

--- app.js
+++ app.js
@@ -2,22 +2,32 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+
+if (process.env.ENV === 'Test') {
+  console.log('This is a test');
+  const db = mongoose.connect('mongodb://localhost/bookAPI_Test');
+} else {
+  console.log('This is for real');
+  const db = mongoose.connect('mongodb://localhost/bookAPI');
+}
+
 
 const port = process.env.PORT || 3000;
-const db = mongoose.connect('mongodb://localhost/bookAPI');
 const Book = require('./models/bookModel');
 const bookRouter = require('./routes/bookRouter')(Book);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my API');
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use('/api', bookRouter);
 
-app.listen(port, (err) => {
-  if (!err) console.log(err);
+app.get('/', (req, res) => {
+  res.send('Welcome to my Nodemon API!');
+});
+
+app.server = app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
+
+module.exports = app;
